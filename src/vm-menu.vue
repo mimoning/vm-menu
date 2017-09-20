09a8cc2251d00bdb5c2ac545cf1c8c7aabddacf0
MimoN <mimo.ouyang@daocloud.io>
2017-09-20 18:34:26 +0800

    feat(menu): 初步完成菜单的功能，位置可随鼠标点击改变

--- src/vm-menu.vue
+++ src/vm-menu.vue
@@ -1,23 +1,47 @@
 <template>
-  <div @contextmenu.prevent="showMenu = true">
-    <span>aaaa</span>
+  <div @contextmenu.prevent="openMenu">
     <slot></slot>
-    <span ref="menu" v-if="showMenu">This is an menu</span>
+    <menu-content ref="menu" v-if="isShow"
+      @close-menu="isShow = false"
+      :position="menuPosition"
+    >
+      <slot name="menu"></slot>
+    </menu-content>
   </div>
 </template>
 <script>
+  import MenuContent from './vm-menu-content'
+
   export default {
+    components: {
+      MenuContent
+    },
     data () {
       return {
-        showMenu: false
+        // 控制菜单是否显示
+        isShow: false,
+        // 控制菜单的位置
+        menuPosition: null
       }
     },
-    beforeCreate () {
-      document.addEventListener('click', e => {
-        const menu = this.$refs.menu
-        // 如果在菜单之外点击，把菜单消除
-        if (!e.path.includes(menu)) {
-          this.showMenu = false
+    methods: {
+      // 打开菜单
+      openMenu (e) {
+        this.isShow = true
+        // 更新菜单位置
+        this.menuPosition = {
+          x: e.clientX,
+          y: e.clientY
+        }
+      }
+    },
+    // 创建时绑定事件，在组件外其他地方右键要把当前组件内的菜单去除
+    created () {
+      document.addEventListener('contextmenu', e => {
+        const trigger = this.$el
+        // 如果发生右键事件的事件冒泡序列中没有当前组件的话，则判断在组件外右键
+        if (!e.path.includes(trigger)) {
+          this.isShow = false
         }
       })
     }
